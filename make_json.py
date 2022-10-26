9e7e96f4b1f8396a2e3436770b0134fb40632930
KrasnovLeonid <45150259+13eav1s@users.noreply.github.com>
2022-10-26 19:31:03 +0300

    Add output of packages that are not in another branch
    Since you first need to display packages that are not in Sisyphus, but in p10, and then vice versa, the get_special_packs function was written, which works for two cases. The function prints to the console the names of packages that are only on the first branch and the number of packages

--- make_json.py
+++ make_json.py
@@ -1,5 +1,6 @@
 import json
 import copy
+import os
 from typing import List
 
 
@@ -42,6 +43,7 @@ def add_to_arr_packs(added_packs_f, array: List[Package], name, epoch, version, 
         added_packs_f.append(name)
         pack = Package(name, epoch, version, release, arch, disttag, buildtime, source)
         array.append(copy.copy(pack))
+        print('adding pack', pack.name, '...', sep='')
 
 
 #  Преобразование пакетов из ветки в объекты класса и запись в массив
@@ -57,6 +59,16 @@ def add_packs_from_file_to_obj_array(file, array, added_packs):
         print(size)
 
 
+#  Получение пакетов ветки которых нет в другой
+def get_special_packs(branch1_names: List[str], branch2_names: List[str], branch1: List[Package]):
+    count_packs = 0
+    for pack_name_index in range(len(branch1_names)):
+        if not (branch1_names[pack_name_index] in branch2_names):
+            print(branch1[pack_name_index])
+            count_packs += 1
+    print(count_packs)
+
+
 data_sisyphus = []
 data_p10 = []
 added_packs_sisyphus = []
@@ -64,17 +76,9 @@ added_packs_p10 = []
 
 add_packs_from_file_to_obj_array('sisyphus', data_sisyphus, added_packs_sisyphus)
 add_packs_from_file_to_obj_array('p10', data_p10, added_packs_p10)
-pass
 
-#  Преобразование пакетов из p10 в объекты класса и запись во множество data_p10
-# with open('p10', 'r') as fj:
-#     str_json = fj.readline()
-#     data = json.loads(str_json)
-#     size = 0
-#     for i in data["packages"]:
-#         pack = Package(i['name'], i['epoch'], i['version'], i['release'], i['arch'], i['disttag'], i['buildtime'], i['source'])
-#         size += 1
-#         data_p10.append(pack)
-#     print(size)
+#  Вывод пакетов которых нет в sisyphus, но есть в p10
+get_special_packs(added_packs_p10, added_packs_sisyphus, data_p10)
 
-#  Вывод пакетов из p10, которых нет в sisyphus
+#  Вывод пакетов которых нет в p10, но есть в sisyphus
+get_special_packs(added_packs_sisyphus, added_packs_p10, data_sisyphus)
