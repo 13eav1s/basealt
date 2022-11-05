da3a45f2c9f25a3ed9984f2f698c10cc8e352690
Борис Сухобрус <bhobrus@gmail.com>
2022-11-05 16:01:02 +0300

    add requirements.txt

--- cli_utility.sh
+++ cli_utility.sh
@@ -6,10 +6,6 @@ echo 'Packages from the sisyphus branch will be written to the sisyphus file'
 touch sisyphus
 echo $'result.JSON - a file that reflects all the packages that are in p10 but not in sisyphus.\nAll packages that are in sisyphus but not in p10.\nAll packages whose version-release is greater in sisyphus than in p10'
 touch result.JSON
-echo 'pip update'
-python3.10 -m pip install --upgrade pip
-echo 'Connecting a library for version comparison'
-pip3 install packaging
 echo 'Writing packages from p10 branch to p10 file'
 curl https://rdb.altlinux.org/api/export/branch_binary_packages/p10 > p10
 echo 'Writing packages from sisyphus branch to sisyphus file'
